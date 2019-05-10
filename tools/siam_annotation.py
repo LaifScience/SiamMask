86d2f0c4c99e925144b584b1a4707ead3dbcbae8
Marius Fersigan <marius.fersigan@gmail.com>
2019-05-10 14:06:00 +0300

    modified:   tools/siam_annotation.py

--- tools/siam_annotation.py
+++ tools/siam_annotation.py
@@ -5,7 +5,6 @@
 # --------------------------------------------------------
 import glob
 import json
-import re
 from tools.test import *
 
 parser = argparse.ArgumentParser(description='PyTorch Tracking Annotation')
@@ -14,14 +13,8 @@ parser.add_argument('--resume', default='', type=str, required=True,
                     metavar='PATH',help='path to latest checkpoint (default: none)')
 parser.add_argument('--config', dest='config', default='config_davis.json',
                     help='hyper-parameter of SiamMask in json format')
-parser.add_argument('--base_path', default='../../data/Catalin_GUN_bright_green_shirt', help='datasets')
+parser.add_argument('--base_path', default='../../data/Multiple_GUN', help='datasets')
 args = parser.parse_args()
-
-
-
-
-
-
 
 if __name__ == '__main__':
     # Setup device
@@ -40,6 +33,7 @@ if __name__ == '__main__':
 
     # Parse Image file
     img_files = sorted(glob.glob(join(args.base_path, '*.jp*')))
+    print(img_files)
     ims = [cv2.imread(imf) for imf in img_files]
 
     # Select ROI
@@ -61,17 +55,22 @@ if __name__ == '__main__':
     selected = False
     bboxes = {}
     for f, im in enumerate(ims):
+        print("processing frame " + str(f))
         tic = cv2.getTickCount()
         if not selected:
             cv2.imshow('SiamMask', im)
             if cv2.waitKey(100000) == ord('s'):  # init
                 x, y, w, h = select_region(im)
+                print("selected region in frame " + str(f))
                 print((x, y, w, h))
                 state = get_state(x, y, w, h)
                 selected = True
-                bboxes[f] = (x, y, x + w, y + h)
+                bboxes[f] = (int(x), int(y), int(x + w), int(y + h))
             elif cv2.waitKey(100000) == ord('n'):
-                print("skip")
+                print("skip frame " + str(f))
+            elif cv2.waitKey(100000) == ord('q'):
+                print("exit at frame " + str(f))
+                break
                 #cv2.imshow('SiamMask', im)
         elif selected:  # tracking
             state = siamese_track(state, im, mask_enable=True, refine_enable=True)  # track
@@ -92,17 +91,17 @@ if __name__ == '__main__':
             cv2.imshow('SiamMask', imcopy)
 
             if cv2.waitKey(100000) == ord('r'):
+                print("rejected proposal for frame " + str(f))
                 cv2.imshow('SiamMask', im)
                 x, y, w, h = select_region(im)
                 state = get_state(x, y, w, h)
-                bboxes[f] = (x, y, x + w, y + h)
-                print("rejected")
+                bboxes[f] = (int(x), int(y), int(x + w), int(y + h))
+                
             elif cv2.waitKey(100000) == ord('a'):
-                print("accepted")
-                print(location)
-                bboxes[f] = (x, y, xw, yh)
+                print("accepted proposal for frame " + str(f))                
+                bboxes[f] = (int(x), int(y), int(xw), int(yh))
             elif cv2.waitKey(100000) == ord('n'):
-                print("reject and next frame")
+                print("reject and next frame in frame " + str(f))
                 selected = False           
             
 
@@ -111,5 +110,5 @@ if __name__ == '__main__':
     fps = f / toc
     print('SiamMask Time: {:02.1f}s Speed: {:3.1f}fps (with visulization!)'.format(toc, fps))
 
-    with open('../../data/Catalin_GUN_bright_green_shirt.json', 'w') as outfile:  
+    with open('../../data/Multiple_GUN.json', 'w') as outfile:  
         json.dump(json.dumps(bboxes), outfile)
